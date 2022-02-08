16e3f52c39e79860740f388db120e2efaf7658b4
saber <saber.tabatabaee@gmail.com>
2022-02-08 22:50:41 +0330

    fix: bug when load avatar in admin

--- resources/views/partials/aside.blade.php
+++ resources/views/partials/aside.blade.php
@@ -10,7 +10,7 @@
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
           <div class="image">
-            <img src="{{Auth()->user()->avatar}}" class="img-circle elevation-2" alt="User Image">
+            <img src="{{Auth()->user()??Auth()->user()->avatar}}" class="img-circle elevation-2" alt="User Image">
           </div>
           <div class="info">
             <a href="{{route('users.profile',Auth()->user()->id)}}" class="d-block">{{Auth()->user()->name}}</a>
